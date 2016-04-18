a779060cd11590a529ebe8dbecbea3378960883b
Jackson Kearl <jkearl@mit.edu>
2016-04-18 03:44:04 -0400

    Initial Commit

---
+++ Graphical/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Graphical
+//
+//  Created by Jackson Kearl on 4/18/16.
+//  Copyright © 2016 Jackson Kearl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
